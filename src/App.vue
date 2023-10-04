eb34539fabb83909bfea3270bb6939ea3d40a274
eliudefrancisco14 <eliudepauloquintasf@gmail.com>
2023-10-04 16:01:01 +0100

    Terminei o dia de hoje

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ListComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListComponent from './components/ListComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListComponent
   }
 }
 </script>
 
 <style>
-#app {
+html{
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
+  background-color: #2c3e50;
 }
 </style>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="list">
+    <div class="container">
+      <div class="row d-flex">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h2>Pesquisar por nome</h2>
+            </div>
+            <hr />
+            <div class="card-body">
+              <div class="card card-content">
+                <div class="row align-content-lg-between">
+                    <div class="col-md-6 d-inline">
+                        imagem
+                    </div>
+                    <div class="col-md-6">
+                        Nome
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ListComponent",
+};
+</script>
+<style>
+hr {
+  width: 500px;
+}
+.container {
+  margin: 0 50vh;
+}
+.card {
+  background-color: #fff;
+  color: #2c3e50;
+  border-radius: 0.4rem;
+  box-shadow: 0px 0px 5px #00000060;
+}
+.card-header,
+.card-body {
+  margin: 0 4rem;
+  padding: 1rem 0;
+}
+</style>
